735e2deec7fba2f57a8d261914dcff343d7ab1fb
tyeong2 <31345464+tyeong2@users.noreply.github.com>
2021-01-11 04:22:35 +0900

    Update naverparser.py

--- naverparser.py
+++ naverparser.py
@@ -7,12 +7,12 @@ import pandas as pd
 
 #네이버 장소검색 정보 파싱
 def info_parser(kwd):
-    baseUrl = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query='
-    url = baseUrl + quote_plus(kwd)
+    baseUrl = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=' #검색을 하면 이동하는 주소
+    url = baseUrl + quote_plus(kwd) #주소에 검색어 추가
     html = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(html, 'html.parser')
 
-    result = []
+    result = [] #결과가 저장될 
     result.append(kwd)
     
     #검색결과에서 정보가 나온다면
@@ -73,13 +73,12 @@ def tag_parser(search):
     html = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(html,'html.parser')
 
-    theme = soup.find_all('div', class_='theme_kwd_area')
-    if theme == []: #테마 키워드가 안 나오는 경우
-        # return ['NaN', 'NaN', 'NaN']
+    theme = soup.find_all('div', class_='theme_kwd_area') #테마 키워드 영역
+    if theme == []: #테마 키워드가 안 나오는 경우        
         return 'NaN'
     list_theme = theme[0].find('ul', class_='list_theme')
     list_item = list_theme.find_all('li',class_='list_item')
-    kwd_list = []
+    kwd_list = [] #키워드가 저장될 리스트
     for ul in list_item:
         li = ul.find_all('span',class_='kwd')
         for i in li:
